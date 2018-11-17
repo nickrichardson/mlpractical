c36c2e05af2d9f2a3f30089e89d5fb1471d43239
Nicholas Richardson <nickrichardson8403@gmail.com>
2018-11-17 11:46:13 +0000

    norm dilation

--- mlp/pytorch_experiment_scripts/model_architectures.py
+++ mlp/pytorch_experiment_scripts/model_architectures.py
@@ -123,7 +123,7 @@ class ConvolutionalNetwork(nn.Module):
                                                              kernel_size=3,
                                                              out_channels=self.num_filters, padding=1,
                                                              bias=self.use_bias,
-                                                             dilation = 4 - i)
+                                                             dilation = i + 2)
 
             out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
             out = F.relu(out)  # apply relu
