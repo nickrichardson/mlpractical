c05c59ffa50e17240d208a25d9d103be4083dcf0
Nicholas Richardson <nickrichardson8403@gmail.com>
2018-11-19 09:57:01 +0000

    attemp to then reduce the dilation

--- mlp/pytorch_experiment_scripts/model_architectures.py
+++ mlp/pytorch_experiment_scripts/model_architectures.py
@@ -120,28 +120,28 @@ class ConvolutionalNetwork(nn.Module):
         out = x
         # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True)
         for i in range(self.num_layers):  # for number of layers times
+            # self.layer_dict['conv_{}'.format(i)] = nn.Conv2d(in_channels=out.shape[1],
+            #                                                  # add a conv layer in the module dict
+            #                                                  kernel_size=3,
+            #                                                  out_channels=self.num_filters, padding=1,
+            #                                                  bias=self.use_bias, stride = self.stride,
+            #                                                  dilation = self.dilation + i*2)
+            #
+            # out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
+            # out = F.relu(out)  # apply relu
+            # print(out.shape)
+
+
             self.layer_dict['conv_{}'.format(i)] = nn.Conv2d(in_channels=out.shape[1],
                                                              # add a conv layer in the module dict
                                                              kernel_size=3,
                                                              out_channels=self.num_filters, padding=3,
                                                              bias=self.use_bias, stride = self.stride,
-                                                             dilation = self.dilation + i*2)
+                                                             dilation = 3 - abs(- 2 + i))
 
             out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
             out = F.relu(out)  # apply relu
             print(out.shape)
-
-
-            # self.layer_dict['conv_{}'.format(i)] = nn.Conv2d(in_channels=out.shape[1],
-            #                                                  # add a conv layer in the module dict
-            #                                                  kernel_size=3,
-            #                                                  out_channels=self.num_filters, padding=3,
-            #                                                  bias=self.use_bias, stride = self.stride,
-            #                                                  dilation = 3 - abs(- 2 + i))
-            #
-            # out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
-            # out = F.relu(out)  # apply relu
-            # print(out.shape)
 
 
 
