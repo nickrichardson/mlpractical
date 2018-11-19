2ee08f492ff3131d426d21fc89ff9002858babd1
Nicholas Richardson <nickrichardson8403@gmail.com>
2018-11-19 15:03:09 +0000

    1232

--- mlp/pytorch_experiment_scripts/model_architectures.py
+++ mlp/pytorch_experiment_scripts/model_architectures.py
@@ -137,7 +137,7 @@ class ConvolutionalNetwork(nn.Module):
                                                              kernel_size=3,
                                                              out_channels=self.num_filters, padding=2,
                                                              bias=self.use_bias,
-                                                                 dilation = 4 - abs(- 2 + i))
+                                                             dilation = 3 - abs(- 2 + i))
 
             out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
             out = F.relu(out)  # apply relu
